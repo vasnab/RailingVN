870b1379797db46af9c9f706e606a846768761eb
vasyl_n <nvotrader@gmail.com>
2019-01-27 01:20:37 +0200

    files renamed

--- RailingVN/Program.cs
+++ RailingVN/Program.cs
@@ -16,7 +16,7 @@ namespace RailingVN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new RailingUIForm());
         }
     }
 }
--- RailingVN/RailingUIForm.cs
+++ RailingVN/RailingUIForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RailingVN
 {
-    public partial class Form1 : Form
+    public partial class RailingUIForm : Form
     {
-        public Form1()
+        public RailingUIForm()
         {
             InitializeComponent();
         }
